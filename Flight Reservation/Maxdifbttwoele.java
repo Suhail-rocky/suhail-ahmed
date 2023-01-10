329cce815f91c29bd740d80c541065ee74370607
Suhail Ahmed <104964358+Suhail-rocky@users.noreply.github.com>
2023-01-10 13:22:43 +0530

    Add files via upload

---
+++ Flight Reservation/Maxdifbttwoele.java
@@ -0,0 +1,37 @@
+/*Input : arr = {2, 3, 10, 6, 4, 8, 1}
+Output : 8
+Explanation : The maximum difference is between 10 and 2.
+
+Input : arr = {7, 9, 5, 6, 3, 2}
+Output : 2
+Explanation : The maximum difference is between 9 and 7.*/
+import java.util.*;
+public class Maxdifbttwoele
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a no of array :");
+		int n = sc.nextInt();
+		int arr[]=new int[n];
+		for(int i=0;i<n;i++)
+		{
+			arr[i]=sc.nextInt();
+		}
+		for(int i=0;i<n;i++)
+		{
+			for(int j=i+1;j<n;j++)
+			{
+				if(arr[i]<arr[j])
+				{
+					int temp = arr[i];
+					arr[i]=arr[j];
+					arr[j]=temp;
+				}
+			}
+		}
+		System.out.println(Arrays.toString(arr));
+		
+
+}
+}
